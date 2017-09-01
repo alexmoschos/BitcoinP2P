953c126833fa51e02edde64f577761513a727285
Alexandros Moschos <alexgmoschos@gmail.com>
2017-09-01 16:55:17 +0300

    Added main record

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
-
-import Lib
+import           Data.Int
+import           Data.Word
+import           Network.Socket
+data Message = Message {
+    mVersion   :: Int32,
+    mServices  :: Word64,
+    mTimestamp :: Int64,
+    mAddrRecv  :: HostName,
+    mAddrFrom  :: HostName,
+    mNonce     :: Word64,
+    mUsrAgent  :: String,
+    mRelay     :: Bool
+}
 
 main :: IO ()
-main = someFunc
+main = return ()
