f018f25c68bfbee14202c14c105498aae82dc7c0
Alex Moschos <alexgmoschos@gmail.com>
2017-09-02 17:47:13 +0300

    serialization for word32 and word64, do the network datatype

--- app/Main.hs
+++ app/Main.hs
@@ -4,25 +4,30 @@ import           Data.Binary
 import           Data.Binary.Put
 import           Data.Int
 --import           Data.Word
+import           Data.Time.Clock.POSIX
 import           Network.Socket
 instance Binary MVersion where
     put MVersion {..} = do
         putWord32le mVersion
-        {-
-        put mServices
-        put mTimestamp
+        putWord64le mServices
+        putWord64le mTimestamp
         put mAddrRecv
         put mAddrFrom
-        put mNonce
+        putWord64le mNonce
         put mUsrAgent
-        put mRelay
-        -}
+        putBoolVal mRelay
     get = undefined
+
+putBoolVal :: Bool -> Put
+putBoolVal True  = putWord8 1
+putBoolVal False = putWord8 0
+
 data MVersion = MVersion {
     mVersion   :: Word32,
     mServices  :: Word64,
-    mTimestamp :: Int64,
+    mTimestamp :: Word64,
     mAddrRecv  :: HostName,
+    --network adress struct needed
     mAddrFrom  :: HostName,
     mNonce     :: Word64,
     mUsrAgent  :: String,
@@ -31,10 +36,10 @@ data MVersion = MVersion {
 
 
 main :: IO ()
-main =
-    let
-        a = MVersion 31900 1 1 "1" "1" 1 "" False
-    in
-        print $ encode a
-        --print $ decode $ encode a
+main = do
+    time <- getPOSIXTime
+    let t = round time
+        a = MVersion 31900 1 t "127.0.0.1" "127.0.0.1" 1 "" False
+    print a
+    encodeFile "a.txt" a
 
